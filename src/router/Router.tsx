c8cbdb347f4c9a92a62ce5c184cc3d337edd1ef2
sohei-fujita <nnn.sohei.fujita@gmail.com>
2024-04-06 21:10:33 +0900

    create header

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -4,22 +4,18 @@ import { Route } from "react-router-dom";
 import { Login } from "../components/pages/Login";
 import { homeRoutes } from "./HomeRoutes";
 import { Page404 } from "../components/pages/Page404";
+import { HeaderLayout } from "../components/templates/HeaderLayout";
 
 export const Router: FC = memo(() => {
   return (
-    // <Routes>
-    //   <Route path="/" element={<Login />} />
-    //   <Route
-    //     path="/home"
-    //     element={homeRoutes.map((route) => (
-    //       <Route key={route.path} path={route.path} element={route.children} />
-    //     ))}
-    //   />
-    // </Routes>
     <Routes>
       <Route path="/" element={<Login />} />
       {homeRoutes.map((route) => (
-        <Route key={route.path} path={route.path} element={route.element} />
+        <Route
+          key={route.path}
+          path={route.path}
+          element={<HeaderLayout>{route.element}</HeaderLayout>}
+        />
       ))}
       <Route path="*" element={<Page404 />} />
     </Routes>
