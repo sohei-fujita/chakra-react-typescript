d5c341a08a65c674f2fafac29d62ac70bd10f935
sohei-fujita <nnn.sohei.fujita@gmail.com>
2024-04-07 11:32:14 +0900

    component化

--- src/components/organisms/layouts/Header.tsx
+++ src/components/organisms/layouts/Header.tsx
@@ -7,12 +7,12 @@ import {
   DrawerOverlay,
   Flex,
   Heading,
-  IconButton,
   useDisclosure,
 } from "@chakra-ui/react";
 import { FC, memo } from "react";
 import { Link } from "react-router-dom";
-import { HamburgerIcon } from "@chakra-ui/icons";
+import { MenuIconButton } from "../../atoms/button/MenuIconButton";
+import { MenuDrawer } from "../../molecules/MenuDrawer";
 
 export const Header: FC = memo(() => {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -43,26 +43,9 @@ export const Header: FC = memo(() => {
           </Box>
           <Link to={"/about"}>設定</Link>
         </Flex>
-        <IconButton
-          aria-label="メニューボタン"
-          icon={<HamburgerIcon />}
-          size="sm"
-          variant="unstyled"
-          display={{ base: "block", md: "none" }}
-          onClick={onOpen}
-        />
+        <MenuIconButton onOpen={onOpen} />
       </Flex>
-      <Drawer placement="left" size="xs" onClose={onClose} isOpen={isOpen}>
-        <DrawerOverlay>
-          <DrawerContent>
-            <DrawerBody p={0} bg="gray.100">
-              <Button w="100%">TOP</Button>
-              <Button w="100%">ユーザー一覧</Button>
-              <Button w="100%">設定</Button>
-            </DrawerBody>
-          </DrawerContent>
-        </DrawerOverlay>
-      </Drawer>
+      <MenuDrawer onClose={onClose} isOpen={isOpen} />
     </>
   );
 });
