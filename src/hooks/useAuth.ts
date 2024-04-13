02644507dc0ce1b509bb45f3ce84a7af9e42807e
sohei-fujita <nnn.sohei.fujita@gmail.com>
2024-04-13 12:08:09 +0900

    add useAuth

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { useCallback, useState } from "react";
+import { User } from "../types/api/user";
+import { useNavigate } from "react-router-dom";
+
+export const useAuth = () => {
+  const navigate = useNavigate();
+
+  const [loading, setLoading] = useState(false);
+
+  const login = useCallback(
+    (id: string) => {
+      axios
+        .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
+        .then((res) => {
+          if (res.data) {
+            navigate("/home");
+          } else {
+            alert("ユーザーが見つかりません");
+          }
+        })
+        .catch(() => alert("ログインできません"))
+        .finally(() => setLoading(false));
+    },
+    [navigate]
+  );
+  return { login };
+};
