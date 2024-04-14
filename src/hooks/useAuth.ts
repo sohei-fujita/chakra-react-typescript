8c0e152d123d959620e117aa4b255941f53bccef
sohei-fujita <nnn.sohei.fujita@gmail.com>
2024-04-14 21:20:27 +0900

    add toast

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,9 +2,11 @@ import axios from "axios";
 import { useCallback, useState } from "react";
 import { User } from "../types/api/user";
 import { useNavigate } from "react-router-dom";
+import { useMessage } from "./useMessage";
 
 export const useAuth = () => {
   const navigate = useNavigate();
+  const { showMessage } = useMessage();
 
   const [loading, setLoading] = useState(false);
 
@@ -16,15 +18,18 @@ export const useAuth = () => {
         .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
         .then((res) => {
           if (res.data) {
+            showMessage({ title: "ログインしました", status: "success" });
             navigate("/home");
           } else {
-            alert("ユーザーが見つかりません");
+            showMessage({ title: "ユーザーが見つかりません", status: "error" });
           }
         })
-        .catch(() => alert("ログインできません"))
+        .catch(() =>
+          showMessage({ title: "ログインできません", status: "error" })
+        )
         .finally(() => setLoading(false));
     },
-    [navigate]
+    [navigate, showMessage]
   );
   return { login, loading };
 };
