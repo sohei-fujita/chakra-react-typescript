2551065d393de2bed01a4117750d5709de6affb9
sohei-fujita <nnn.sohei.fujita@gmail.com>
2024-04-17 20:24:21 +0900

    add name card

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,36 @@
+import { Box, Image, Stack, Text, Wrap, WrapItem } from "@chakra-ui/react";
 import { FC, memo } from "react";
 
 export const UserManagement: FC = memo(() => {
-  return <p>ユーザー管理ページです</p>;
+  return (
+    <Wrap p={{ base: 4, md: 10 }}>
+      <WrapItem>
+        <Box
+          w="260px"
+          h="260px"
+          bg="white"
+          borderRadius="10px"
+          shadow="md"
+          p={4}
+          _hover={{ cursor: "pointer", opacity: 0.8 }}
+        >
+          <Stack textAlign="center">
+            <Image
+              borderRadius="full"
+              boxSize="160px"
+              src="https://source.unsplash.com/random"
+              alt="プロフィール"
+              m="auto"
+            />
+            <Text fontSize="lg" fontWeight="bold">
+              ふじた
+            </Text>
+            <Text fontSize="sm" color="gray">
+              FUJITA
+            </Text>
+          </Stack>
+        </Box>
+      </WrapItem>
+    </Wrap>
+  );
 });
